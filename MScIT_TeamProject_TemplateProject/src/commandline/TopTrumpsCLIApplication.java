5cfb49e350f596fa0f8d07e023910840543d9d00
rosiesinead <2225814t@student.gla.ac.uk>
2019-01-30 11:02:49 +0000

    commandline updated with testlog

--- MScIT_TeamProject_TemplateProject/src/commandline/TopTrumpsCLIApplication.java
+++ MScIT_TeamProject_TemplateProject/src/commandline/TopTrumpsCLIApplication.java
@@ -15,10 +15,12 @@ public class TopTrumpsCLIApplication {
 	 */
 	public static void main(String[] args) {
 
-		boolean writeGameLogsToFile = false; // Should we write game logs to file?
+		boolean writeTestLog = false; // Should we write game logs to file?
 
 		if (args[0].equalsIgnoreCase("true"))
-			writeGameLogsToFile = true; // Command line selection
+			writeTestLog = true; // Command line selection
+
+		TestLog testLog = new TestLog(writeTestLog); // Create logger. If writeTestLog is false, no logs will be written
 
 		// State
 		boolean userWantsToQuit = false; // flag to check whether the user wants to quit the application
